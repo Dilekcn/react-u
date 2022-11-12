fde0ea86fc07708f395cfd98483e6aec2b81eb01
Dilekcn <canturkdk@gmail.com>
2022-11-12 19:48:26 +0000

    navigator

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import SingelComment from "./SingelComment";
 import UserCart from "./UserCart";
 
 function App() {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  );
+
   return (
     <div className="ui cards">
       <UserCart>
---
+++ src/DisplayImg.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class DisplayImg extends Component {
+  render() {
+    return <div>DisplayImg</div>;
+  }
+}
